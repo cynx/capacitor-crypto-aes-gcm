c9308b8a97e94af5f7e3d3293637322d07174ff8
Vishal Singh <ivishalsingh1986@gmail.com>
2022-02-16 11:41:54 +0800

    adds error handling

--- ios/Plugin/Crypto.swift
+++ ios/Plugin/Crypto.swift
@@ -31,36 +31,41 @@ extension SymmetricKey {
 @available(iOS 13.0, *)
 @objc public class Crypto: NSObject {
 
-    @objc public func encrypt(text: String, base64Encoded: String) -> String {
+    @objc public func encrypt(text: String, base64Encoded: String, completionHandler: (_ result: String, _ success: Bool) ->Void ) {
         do {
-            guard let deserialisedSymmetricKey = SymmetricKey(base64EncodedString: base64Encoded) else { 
-                return "Could not decode key: \(base64Encoded)" 
+            guard let deserialisedSymmetricKey = SymmetricKey(base64EncodedString: base64Encoded) else {
+                completionHandler("Could not decode key: \(base64Encoded)", false)
+                return
             }
             let textData = text.data(using: .utf8)!
             let encrypted = try AES.GCM.seal(textData, using: deserialisedSymmetricKey)
-            return encrypted.combined!.base64EncodedString()}
-        catch {
-            return "Error encrypting"
+            completionHandler(encrypted.combined!.base64EncodedString(), true)
+        }
+        catch let error {
+            completionHandler("Error encrypting message: \(error.localizedDescription)", false)
         }
     }
 
-     @objc public func decrypt(text: String, base64Encoded: String) -> String {
+    @objc public func decrypt(text: String, base64Encoded: String, completionHandler:(_ result: String, _ success: Bool) ->Void) {
        do {
             guard let deserialisedSymmetricKey = SymmetricKey(base64EncodedString: base64Encoded) else {
-                return "Could not decode key: \(base64Encoded)"
+                completionHandler("Could not decode key: \(base64Encoded)", false)
+                return
             }
             guard let data = Data(base64Encoded: text) else {
-                return "Could not decode text: \(text)"
+                completionHandler("Could not decode key: \(text)", false)
+                return
             }
             let sealedBox = try AES.GCM.SealedBox(combined: data)
             let decryptedData = try AES.GCM.open(sealedBox, using: deserialisedSymmetricKey)
             guard let text = String(data: decryptedData, encoding: .utf8) else {
-                return "Could not decode data: \(decryptedData)"
+                completionHandler("Could not decode key: \(decryptedData)", false)
+                return
             }
-            return text
+           completionHandler(text, true)
         }
         catch let error {
-            return "Error decrypting message: \(error.localizedDescription)"
+            completionHandler ("Error decrypting message: \(error.localizedDescription)", false)
         }
     }
 
